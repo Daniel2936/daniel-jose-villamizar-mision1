16d85060a413e200e74a237931a71bfb8be2ee48
Daniel2936 <danielvillamizar72@gmail.com>
2023-03-22 13:55:58 -0500

    22-marzo-2  commit

---
+++ Mision 3/registro.js
@@ -0,0 +1,70 @@
+class Usuario{
+    nombre 
+    apellidos
+    area
+    usuario
+    edad
+    direccion
+    email
+
+    constructor(nombre,apellidos,area,usuario,edad,direccion,email){
+        this.nombre=nombre
+        this.apellidos=apellidos
+        this.area=area
+        this.usuario=usuario
+        this.edad=edad
+        this.direccion=direccion
+        this.email=email
+    }
+
+}
+
+
+
+const nombres=document.getElementById('nombres')
+const apellidos=document.getElementById('apellidos')
+const area=document.getElementById('area')
+const usuarioo=document.getElementById('usuario')
+const edad=document.getElementById('edad')
+const direccion=document.getElementById('direccion')
+const email=document.getElementById('email')
+const formulario=document.getElementById('formulario')
+let listaP=[]
+let usuarios=[]
+
+
+formulario.addEventListener("submit", form);
+function form(event) {
+    event.preventDefault();
+
+
+
+    
+    const usero=new Usuario(nombres.value,apellidos.value,area.value,usuarioo.value,edad.value,direccion.value,email.value )
+    
+    const {usuario}=usero
+
+    if(usuarios.includes(usuario)){
+        alert('este usuario ya esta tomado, cambielo')
+        usuarioo.value=""
+    }else{
+
+        usuarios.push(usuario)
+        listaP.push(usero)
+        console.log(listaP)
+        localStorage.setItem("Usuarios", JSON.stringify(listaP))
+        nombres.value=""
+        apellidos.value=""
+        area.value=""
+        usuarioo.value=""
+        edad.value=""
+        direccion.value=""
+        email.value=""
+
+    }
+
+}
+
+window.onbeforeunload = () => {
+    localStorage.clear();
+  }
---
+++ Mision 3/usuarios.js
@@ -0,0 +1,3 @@
+
+let usuarios=JSON.parse(localStorage.getItem('Usuarios'))
+console.log(usuarios)
